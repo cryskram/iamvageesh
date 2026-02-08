e4b2ee68ddfeccceb56db7416caa8fbadbf2d901
cryskram <vageeshgn2005@gmail.com>
2026-02-08 14:43:30 +0530

    update: footer text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
+import { FaCopyright, FaHeart, FaRegCopyright } from "react-icons/fa6";
+
 const Footer = () => {
   return (
     <footer className="text-center py-8 text-slate-100 text-sm bg-slate-900">
-      <p>&copy; {new Date().getFullYear()} Vageesh G N. Built with ❤️</p>
+      <p className="inline-flex items-center gap-1 justify-center">
+        <FaRegCopyright /> {new Date().getFullYear()} Vageesh G N. Built with{" "}
+        <FaHeart />
+        and code
+      </p>
     </footer>
   );
 };
